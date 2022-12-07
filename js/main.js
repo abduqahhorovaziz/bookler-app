b1fb43e4eacbfb53e2fdacb8ade59ae2eba1f538
abduqahhorovaziz <83882515+abduqahhorovaziz@users.noreply.github.com>
2022-12-07 19:10:46 +0500

    feat: scroll function is ready

--- js/main.js
+++ js/main.js
@@ -2,9 +2,14 @@ const sidebar = document.querySelector("#sidebar");
 const toggleBtn = document.querySelector("#toggleBtn");
 const logoBrand = document.querySelector("#logoBrand");
 const sidebarTop = document.querySelector("#sidebarTop");
-const navItems = document.querySelectorAll('.nav-item')
-const navLinks = document.querySelectorAll('.nav-link')
-const navIcons = document.querySelectorAll('.nav-icon')
+const navItems = document.querySelectorAll(".nav-item");
+const navLinks = document.querySelectorAll(".nav-link");
+const navIcons = document.querySelectorAll(".nav-icon");
+const tabSection = document.querySelector("#tabSection");
+const tabContainer = document.querySelector("#tabContainer");
+const tabs = document.querySelectorAll("[data-tab-value]");
+const tabInfos = document.querySelectorAll("[data-tab-info]");
+
 
 toggleBtn.onclick = function () {
   sidebar.classList.toggle("active");
@@ -15,23 +20,105 @@ toggleBtn.onclick = function () {
   if (sidebarTop.classList.contains("justify-between")) {
     sidebarTop.classList.remove("justify-between");
     sidebarTop.classList.add("justify-center");
+  } else {
+    sidebarTop.classList.add("justify-between");
   }
-  else{
-    sidebarTop.classList.add('justify-between')
-  }
 
-  navItems.forEach(function(item){
-    item.classList.toggle('hidden')
-  })
+  navItems.forEach(function (item) {
+    item.classList.toggle("hidden");
+  });
 
-  navLinks.forEach(function(item){
-    item.classList.toggle('px-[19px]')
-    item.classList.toggle('px-[10px]')
-    item.classList.toggle('flex-center')
-  })
+  navLinks.forEach(function (item) {
+    item.classList.toggle("px-[19px]");
+    item.classList.toggle("px-[10px]");
+    item.classList.toggle("flex-center");
+  });
 
-  navIcons.forEach(function(item){
-    item.classList.toggle('mr-[13px]')
-  })
+  navIcons.forEach(function (item) {
+    item.classList.toggle("mr-[13px]");
+  });
 
+  // tabSection.classList.toggle('max-w-[1400px]')
+  tabContainer.classList.toggle("max-w-[1030px]");
+  tabContainer.classList.toggle("max-w-[95%]");
 };
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", function(){
+    if (this.classList.contains("tab-activee")) {
+      this.classList.remove("tab-activee");
+    } else {
+      for(let el of tabs){
+        el.classList.remove('tab-activee')
+      }
+      this.classList.add("tab-activee");
+    }
+
+
+    const target = document.querySelector(tab.dataset.tabValue);
+
+    tabInfos.forEach((tabInfo) => {
+      tabInfo.classList.remove("tab-active");
+      target.classList.add("tab-active");
+    });
+  });
+});
+
+// window.addEventListener('scroll', function(el){
+//   console.log(Math.floor(window.scrollY),'fpp');
+// })
+
+window.onscroll = function() {
+  let header = document.querySelector('#header');
+  let tabNav = document.querySelector('#tabNav');
+  let plane = document.querySelector('.airplane');
+  let titles = document.querySelectorAll('.tab-title')
+  let tabBtn = document.querySelectorAll('.tab-btn')
+  let smallTab = document.querySelectorAll('.smallTab')
+  let largeTab = document.querySelectorAll('.largeTab')
+
+  if(window.pageYOffset > 150){
+    header.classList.remove('h-[491px]')
+    header.classList.add('h-[117px]')
+    header.classList.add('z-40')
+    tabNav.classList.add('fixed')
+    plane.classList.add('opacity-0')
+    titles.forEach(title => {
+      title.classList.add('hidden')
+    })
+    tabBtn.forEach(el => {
+      el.classList.remove('w-[68px]')
+      el.classList.remove('h-[73px]')
+      el.classList.add('w-[46px]')
+      el.classList.add('h-[46px]')
+    })
+    smallTab.forEach(el => {
+      el.classList.remove('hidden')
+    })
+    largeTab.forEach(el => {
+      el.classList.add('hidden')
+    })
+
+  } else{
+    header.classList.remove('h-[117px]')
+    header.classList.add('h-[491px]')
+    header.classList.remove('z-40')
+    tabNav.classList.remove('fixed')
+    plane.classList.remove('opacity-0')
+    titles.forEach(title => {
+      title.classList.remove('hidden')
+    })
+    tabBtn.forEach(el => {
+      el.classList.remove('w-[46px]')
+      el.classList.remove('h-[46px]')
+      el.classList.add('w-[68px]')
+      el.classList.add('h-[73px]')
+    })
+    smallTab.forEach(el => {
+      el.classList.add('hidden')
+    })
+    largeTab.forEach(el => {
+      el.classList.remove('hidden')
+    })
+  }
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,22 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      animation:{
+        'tab': 'tab .2s ease-in'
+      },
+      keyframes: {
+        tab: {
+          '0%': {opacity:0 },
+          '100%': { opacity:100 },
+        }
+      },
       fontFamily: {
         'open': ['Open Sans', 'sans-serif']
       },
       boxShadow: {
-        'sidebar-shadow': ['0px 12px 18px rgba(11, 91, 220, 0.1)']
+        'sidebar-shadow': ['0px 12px 18px rgba(11, 91, 220, 0.1)'],
+        'tab-shadow': ['0px 20px 40px rgba(0, 0, 0, 0.04)'],
+        'tag-shadow': ['0px 15px 30px rgba(255, 255, 255, 0.0960719'],
       }
     },
   },
